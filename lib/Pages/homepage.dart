4cb8e5656aa26a8a7c473d982d74a23bec049a1d
sp13011996 <githubprojects0508@gmail.com>
2022-06-15 20:51:30 +0530

    initial commit

---
+++ lib/Pages/homepage.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:news_rest_api/models/newsinfo.dart';
+import 'package:news_rest_api/services/api_manager.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late Future<Welcome> _newsModel;
+
+  @override
+  void initState() {
+    _newsModel = API_Manager().getNews();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('News App'),
+      ),
+      body: Container(
+        child: FutureBuilder<Welcome>(
+          future: _newsModel,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.articles.length,
+                  itemBuilder: (context, index) {
+                    var article = snapshot.data!.articles[index];
+                    return Container(
+                      height: 100,
+                      margin: const EdgeInsets.all(8),
+                      child: Row(
+                        children: <Widget>[
+                          Card(
+                            clipBehavior: Clip.antiAlias,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            child: AspectRatio(
+                              aspectRatio: 1,
+                              child: Image.network(article.urlToImage ?? '',
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                          SizedBox(width: 20),
+                          Flexible(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  article.title ?? '',
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                Text(
+                                  article.description ?? '',
+                                  overflow: TextOverflow.ellipsis,
+                                  maxLines: 2,
+                                )
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                    );
+                  });
+            } else {
+              return Center(child: CircularProgressIndicator());
+            }
+          },
+        ),
+      ),
+    );
+  }
+}
